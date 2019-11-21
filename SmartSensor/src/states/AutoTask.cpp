b91a0c4a91962331f84382082abb488e4bda37e0
Riccardo Romagnoli <riccardo.romagnoli3@studio.unibo.it>
2019-11-21 15:36:26 +0100

    Progress autoTask

--- SmartSensor/src/states/AutoTask.cpp
+++ SmartSensor/src/states/AutoTask.cpp
@@ -1,8 +1,17 @@
 #include "AutoTask.h"
 #include "Arduino.h"
 
-AutoTask::AutoTask(){
-  
+#define OFFSET (ANGLE / 2)
+
+//Gestione allarme
+//scan oraria e anti oraria
+//raccolta dati, invio dati
+//spegnere servo al cambio modalità
+
+AutoTask::AutoTask(Task* blinkTask){
+  this->blinkTask = blinkTask;
+  this->sonar = new Sonar(TRIGSONAR, ECHOSONAR);
+  this->servo = new ServoMotorImpl(SERVOMOTOR);
 }
   
 void AutoTask::init(int period){
@@ -11,9 +20,48 @@ void AutoTask::init(int period){
 }
 
 void AutoTask::init(){
-
+  for(int i = 0; i < POSITIONS; i++){
+    results[i] = 0;
+  }
+  actualPosition = 0;
+  clockwise = true;
+  servo->on();
 }
   
 void AutoTask::tick(){
   Serial.println("Modalita auto attiva");
+  checkObject();
+  servo->setPosition(ANGLE * actualPosition + OFFSET);
+  clockwise ? ++actualPosition : --actualPosition;
+  checkEndScan();
+}
+
+void AutoTask::setAlarm(bool status){
+  alarm = status;
+  alarmClockwise = clockwise;
+  this->blinkTask->setActive(status);
+}
+
+void AutoTask::checkEndScan(){
+  if(actualPosition == -1 || actualPosition == POSITIONS){
+    clockwise = !clockwise;
+  }
+  if(alarm && clockwise == alarmClockwise )
+    this->setAlarm(false);
+}
+
+void AutoTask::checkObject(){
+  float distance = sonar->sonarScan(); 
+
+  if(distance >= D_MIN && distance <= D_MAX){
+    //Object detected
+    this->setAlarm(true);
+    this->results[actualPosition] = distance;
+  }else if (distance > D_MAX){
+    //Object not detected
+  }else if (distance < D_MIN){
+    //Alarm With Tracking
+    alarmTracking = true;
+    this->results[actualPosition] = distance;
+  }
 }
--- SmartSensor/src/states/AutoTask.h
+++ SmartSensor/src/states/AutoTask.h
@@ -2,14 +2,32 @@
 #define __AUTOTASK__
 
 #include "Task.h"
+#include "./pir/Pir.h"
+#include "./servo/servo_motor_impl.h"
+#include "./sonar/Sonar.h"
+#include "./macros.h"
 
 class AutoTask: public Task {
 
 public:
-  AutoTask();
+  AutoTask(Task* blinkTask);
   void init(int period); 
   void init();
   void tick();
+private:
+  Sonar* sonar;
+  ServoMotor* servo;
+  Task* blinkTask;
+  int actualPosition;
+  bool clockwise;
+  bool alarm;
+  bool alarmClockwise;
+  bool alarmTracking;
+  float results[POSITIONS];
+
+  void setAlarm(bool status);
+  void checkObject();
+  void checkEndScan();
 };
 
 #endif
