7832e781258ee028175aaecfa333547e6416c759
Riccardo Romagnoli <riccardo.romagnoli3@studio.unibo.it>
2020-02-13 12:56:09 +0100

    Basic Implementation Edge

--- Progetto 3/src/sd-edge/src/main.cpp
+++ Progetto 3/src/sd-edge/src/main.cpp
@@ -1,23 +1,82 @@
-#include "user_config.h"
 #include "Arduino.h"
+#include "led/Led.h"
+#include "potenz/Potenziometro.h"
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
 
-#define LEDERR_PIN 3
-#define LEDOK_PIN 4
+#define LEDERR_PIN D4
+#define LEDOK_PIN D3
 #define POT_PIN A0
 
+Potenziometro* pot;
+Light* ledErr;
+Light* ledOk;
+
+/* wifi network name */
+String ssidName = "littlebarfly";
+/* WPA2 PSK password */
+String pwd = "seiot1920";
+/* service IP address */ 
+String address = "http://0dfeb236.ngrok.io";
+
 void setup() {                
-  pinMode(LEDOK_PIN, OUTPUT); 
-  Serial.begin(115200);     
-  Serial.println("READY"); 
+  Serial.begin(9600);
+  ledOk = new Led(LEDOK_PIN);
+  ledErr = new Led(LEDERR_PIN);
+  pot = new Potenziometro(POT_PIN);
+  
+  ledOk->switchOff();
+  ledErr->switchOff();
+
+  WiFi.begin(ssidName, pwd);
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {  
+    delay(500);
+    Serial.print(".");
+  } 
+  Serial.println("Connected: \n local IP: "+WiFi.localIP());
+}
+
+int sendData(String address, float value, String place){  
+   HTTPClient http;    
+   http.begin(address + "/api/data");      
+   http.addHeader("Content-Type", "application/json");     
+   String msg = 
+    String("{ \"value\": ") + String(value) + 
+    ", \"place\": \"" + place +"\" }";
+   int retCode = http.POST(msg);   
+   http.end();  
+      
+   // String payload = http.getString();  
+   // Serial.println(payload);      
+   return retCode;
 }
 
 void loop() {
-  int value = analogRead(POT_PIN);
+  int value = pot->readPotenziometro();
   Serial.println("Value: " + String(value));
-  digitalWrite(LEDOK_PIN, HIGH);
+  ledOk->switchOn();
   Serial.println("ON");
   delay(500);             
-  digitalWrite(LEDOK_PIN, LOW);
+  ledOk->switchOff();
   Serial.println("OFF");   
-  delay(500);              
+  delay(500);
+
+  if (WiFi.status()== WL_CONNECTED){   
+    /* read sensor */
+    float value = (float) analogRead(A0) / 1023.0;
+    
+    /* send data */
+    Serial.println("sending "+String(value)+"...");
+    int code = sendData(address, value, "home");
+
+    /* log result */
+    if (code == 200){
+      Serial.println("ok");
+    } else {
+      Serial.println("error");
+    }
+  } else { 
+    Serial.println("Error in WiFi connection");   
+  }    
 }
